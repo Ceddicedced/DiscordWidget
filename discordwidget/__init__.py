f7c964643be044cc43617bbfa6c4135559367abb
Cedric Busacker <git@busacker.dev>
2024-07-02 19:13:33 +0200

    # Ruff fix

--- discordwidget/__init__.py
+++ discordwidget/__init__.py
@@ -1,6 +1,8 @@
-from .widget import Widget
-from .member import Member
-from .channel import Channel
+"""DiscordWidget API Wrapper."""
+
+from .widget import Widget  # noqa: F401
+from .member import Member  # noqa: F401
+from .channel import Channel  # noqa: F401
 
 __version__ = "0.2.0"
 __author__ = "Ceddicedced"
